dca5bb1ef372b6fa408c079314bda0bcedb497c6
19anurag97 <19anurag97@gmail.com>
2023-02-11 13:40:12 +0530

    accessing registers from memory(0x2A)

---
+++ avr328p/avrblink01/avrblink01/main.c
@@ -0,0 +1,73 @@
+/*
+ * avr_01.c
+ *
+ * Created: 11-02-2023 11:46:01
+ * Author : 19anu
+ */ 
+#define F_CPU 8000000UL
+#define direction_D		*((volatile uint8_t*)0x2A)
+#define port_D			*((volatile uint8_t*)0x2B)
+
+
+#include <avr/io.h>
+#include <util/delay.h>
+#include <stdint.h>
+
+void function1(void);//blink using delay in util/delay.h
+void function2(void);
+void function3(void);
+
+int main(void)
+{
+	//function1();
+/*	function2();*/
+	function3();
+}
+
+void function1(void)
+{
+	DDRD |= (1 << 0);//0xFF;
+	while (1)
+	{
+		PORTD |= (1 << 0);
+		_delay_ms(1000);
+		PORTD &= ~(1 << 0);
+		_delay_ms(1000);
+	}
+}
+
+void function2(void)
+{	//artificial delay()
+	
+	DDRD |= (1 << 0);//0xFF;
+	while (1)
+	{
+		for (long i=0; i<100000; i++)
+		{
+			PORTD |= (1 << 0);
+		}
+		for (long i=0; i<100000; i++)
+		{
+			PORTD &= ~(1 << 0);
+		}	
+	}	
+}
+
+void function3(void)
+{
+	//0x0B (0x2B) PORTD
+	//0x0A (0x2A) DDRD
+	//0x09 (0x29) PIND
+// 	volatile uint8_t* direction_D = (volatile uint8_t*)0x2A;
+// 	volatile uint8_t* port_D = (volatile uint8_t*)0x2B;
+	//MACROS define
+	
+	direction_D = 1;
+	while (1)
+	{
+		port_D = 1;
+		_delay_ms(100);
+		port_D = 0;
+		_delay_ms(100);
+	}
+}
