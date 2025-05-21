8b8c028de8ef758b2da38158168bc57948c494a4
AnuragBisht <19anurag97@gmail.com>
2025-05-21 23:43:43 +0530

    Compilation successful with STMCubeIDE. -> Added FreeRTOS in ThirdParty lib and included PATH    settings. -> Conflicts related to FreeRTOSConfig.h file resolved    (SVC Handler, TickHook, StackOverflow errors) -> Using STM HAL System Clock, set for Timer6.

---
+++ STM_RTOS/RTOS_Workspace/RTOS_Work/Debug/ThirdParty/FreeRtos/list.d
@@ -0,0 +1,17 @@
+ThirdParty/FreeRtos/list.o: ../ThirdParty/FreeRtos/list.c \
+ C:/Users/19anu/OneDrive/Desktop/Github_projects/mcu_coding/STM_RTOS/RTOS_Workspace/RTOS_Work/ThirdParty/FreeRtos/include/FreeRTOS.h \
+ C:/Users/19anu/OneDrive/Desktop/Github_projects/mcu_coding/STM_RTOS/RTOS_Workspace/RTOS_Work/ThirdParty/FreeRtos/FreeRTOSConfig.h \
+ C:/Users/19anu/OneDrive/Desktop/Github_projects/mcu_coding/STM_RTOS/RTOS_Workspace/RTOS_Work/ThirdParty/FreeRtos/include/projdefs.h \
+ C:/Users/19anu/OneDrive/Desktop/Github_projects/mcu_coding/STM_RTOS/RTOS_Workspace/RTOS_Work/ThirdParty/FreeRtos/include/portable.h \
+ C:/Users/19anu/OneDrive/Desktop/Github_projects/mcu_coding/STM_RTOS/RTOS_Workspace/RTOS_Work/ThirdParty/FreeRtos/include/deprecated_definitions.h \
+ C:/Users/19anu/OneDrive/Desktop/Github_projects/mcu_coding/STM_RTOS/RTOS_Workspace/RTOS_Work/ThirdParty/FreeRtos/portable/GCC/ARM_CM4F/portmacro.h \
+ C:/Users/19anu/OneDrive/Desktop/Github_projects/mcu_coding/STM_RTOS/RTOS_Workspace/RTOS_Work/ThirdParty/FreeRtos/include/mpu_wrappers.h \
+ C:/Users/19anu/OneDrive/Desktop/Github_projects/mcu_coding/STM_RTOS/RTOS_Workspace/RTOS_Work/ThirdParty/FreeRtos/include/list.h
+C:/Users/19anu/OneDrive/Desktop/Github_projects/mcu_coding/STM_RTOS/RTOS_Workspace/RTOS_Work/ThirdParty/FreeRtos/include/FreeRTOS.h:
+C:/Users/19anu/OneDrive/Desktop/Github_projects/mcu_coding/STM_RTOS/RTOS_Workspace/RTOS_Work/ThirdParty/FreeRtos/FreeRTOSConfig.h:
+C:/Users/19anu/OneDrive/Desktop/Github_projects/mcu_coding/STM_RTOS/RTOS_Workspace/RTOS_Work/ThirdParty/FreeRtos/include/projdefs.h:
+C:/Users/19anu/OneDrive/Desktop/Github_projects/mcu_coding/STM_RTOS/RTOS_Workspace/RTOS_Work/ThirdParty/FreeRtos/include/portable.h:
+C:/Users/19anu/OneDrive/Desktop/Github_projects/mcu_coding/STM_RTOS/RTOS_Workspace/RTOS_Work/ThirdParty/FreeRtos/include/deprecated_definitions.h:
+C:/Users/19anu/OneDrive/Desktop/Github_projects/mcu_coding/STM_RTOS/RTOS_Workspace/RTOS_Work/ThirdParty/FreeRtos/portable/GCC/ARM_CM4F/portmacro.h:
+C:/Users/19anu/OneDrive/Desktop/Github_projects/mcu_coding/STM_RTOS/RTOS_Workspace/RTOS_Work/ThirdParty/FreeRtos/include/mpu_wrappers.h:
+C:/Users/19anu/OneDrive/Desktop/Github_projects/mcu_coding/STM_RTOS/RTOS_Workspace/RTOS_Work/ThirdParty/FreeRtos/include/list.h:
